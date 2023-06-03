eb5afefe1b50ffd36db4c33384c907746c8cfdb1
kopanbor <kopanbor@students.zhaw.ch>
2023-06-03 11:37:44 +0200

    Testing ScoreKeeping Team WIN

--- src/test/java/com/test/demo/test/ScoreKeeperTest.java
+++ src/test/java/com/test/demo/test/ScoreKeeperTest.java
@@ -2,15 +2,39 @@ package com.test.demo.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ScoreKeeperTest {
     private ScoreKeeper scoreKeeper;
 
+    @BeforeEach
+    public void setUp() {
+        scoreKeeper = new ScoreKeeper();
+    }
+
     @Test
     public void testInitalScore() {
         scoreKeeper = new ScoreKeeper();
         assertEquals("000:000", scoreKeeper.getScore());
+    }
+
+    @Test
+    public void testScoreTeamWIN1() {
+        scoreKeeper.scoreTeamWIN1();
+        assertEquals("001:000", scoreKeeper.getScore());
+    }
+
+    @Test
+    public void testScoreTeamWIN2() {
+        scoreKeeper.scoreTeamWIN2();
+        assertEquals("002:000", scoreKeeper.getScore());
+    }
+
+    @Test
+    public void testScoreTeamWIN3() {
+        scoreKeeper.scoreTeamWIN3();
+        assertEquals("003:000", scoreKeeper.getScore());
     }
 
 }
