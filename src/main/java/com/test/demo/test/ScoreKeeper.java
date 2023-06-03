4c70719d852160ad7e7ceaf57e003db9723b2d68
kopanbor <kopanbor@students.zhaw.ch>
2023-06-03 11:26:30 +0200

    Javafile erstellt

---
+++ src/main/java/com/test/demo/test/ScoreKeeper.java
@@ -0,0 +1,12 @@
+package com.test.demo.test;
+
+public class ScoreKeeper {
+    private int scoreTeamWIN;
+    private int scoreTeamBWL;
+
+    public ScoreKeeper(){
+        scoreTeamWIN = 0;
+        scoreTeamBWL = 0;
+    };
+    
+}
